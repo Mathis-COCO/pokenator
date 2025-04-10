d69f32c75048aecd3361b12e8c6b4a77854c8c6e
Donoma <dorianlevee30@gmail.com>
2025-04-10 08:56:42 +0200

    adding win conditions

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -43,7 +43,17 @@ interface ChatBody {
 }
 
 function Homepage() {
-    const pokemonNamesList = ['Bulbizarre', 'Salamèche', 'Carapuce', 'Pikachu', 'Rondoudou', 'Mewtwo', 'Mew', 'Groudon', 'Embrylex', 'Léviator']; // Liste des noms de Pokémon à afficher
+    const iaContext: ChatBody = {
+        "messages": [{
+            "role": "system",
+            "content": `Tu joues à un jeu avec moi. Voici les règles, je vais penser à un pokemon, tu dois deviner lequel` +
+            `en me posant des questions à laquelle je devrais repondre par "oui", "non" ou "je sais pas". Les nom des pokemons seront en français.` +
+            `La réponse devra être dans un format json. Un 1er attribut nommé "type" contenant soit "question" si le contenu est une question,` +
+            `soit "answer" si le contenu est une tentative à trouver un pokemon. Le second attribut "content" contiendra le contenu de ta question ou reponse.` +
+            `Lorsque le type est "answer" le content contiendra uniquement le nom du pokemeon et rien d'autre.Ta reponse devra comporter uniquement le json.`
+        }]
+    };
+    const [ pokemonNamesList, setPokemonNameList ] = useState<String[]>([]);
     const [pokemonData, setPokemonData] = useState<PokemonData[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<ErrorType | null>(null);
@@ -51,49 +61,9 @@ function Homepage() {
     const [selectedPokemon, setSelectedPokemon] = useState<PokemonData | null>(null);
     const [isPopupOpen, setIsPopupOpen] = useState(false);
     const [currentQuestion, setCurrentQuestion] = useState<string>('');
-    const [bodyPokemon, setBodyPokemon] = useState<ChatBody>({
-      "messages": [{
-          "role": "system",
-          "content": "Tu joues à un jeu avec moi. Voici les règles, je vais penser à un pokemon, tu dois deviner lequel en me posant des questions à laquelle je devrais repondre par \"oui\", \"non\" ou \"je sais pas\". Il ne devra pas y avoir de commentaire inutile dan ta question. Les nom des pokemon seront en français."
-      }]
-  });
+    const [bodyPokemon, setBodyPokemon] = useState<ChatBody>(iaContext);
 
     useEffect(() => {
-        const fetchPokemonData = async () => {
-            setLoading(true);
-            setError(null);
-
-            try {
-                const response = await fetch('https://tyradex.vercel.app/api/v1/pokemon');
-                if (!response.ok) {
-                    const errorMessage = `Erreur HTTP! Statut: ${response.status}`;
-                    console.error('Erreur lors de la récupération des données Pokémon:', errorMessage);
-                    throw new Error(errorMessage);
-                }
-                const allPokemon: PokemonData[] = await response.json();
-                console.log('Données Pokémon récupérées avec succès:', allPokemon);
-
-                const filteredPokemon = allPokemon.filter((pokemon) =>
-                    pokemonNamesList.some(
-                        (name) => pokemon.name.fr === name || pokemon.name.en === name
-                    )
-                );
-
-                setPokemonData(filteredPokemon);
-            } catch (e: any) {
-                let message = 'Une erreur inconnue est survenue lors de la récupération des données Pokémon.';
-                if (e instanceof Error) {
-                    message = e.message;
-                } else if (typeof e === 'string') {
-                    message = e;
-                }
-                setError({ message });
-                console.error('Erreur lors de la récupération des données Pokémon:', e);
-            } finally {
-                setLoading(false);
-            }
-        };
-
         const fetchTypeIcons = async () => {
             try {
                 const response = await fetch('https://pokebuildapi.fr/api/v1/types');
@@ -104,16 +74,50 @@ function Homepage() {
                 }
                 const data = await response.json();
                 setTypeIcons(data);
-                console.log('Icônes de type récupérées avec succès:', data);
             } catch (error) {
                 console.error('Erreur lors de la récupération des icônes de type:', error);
             }
         };
 
-        fetchPokemonData();
+        fetchPokemonData(pokemonNamesList);
         fetchTypeIcons();
         postAnswer(bodyPokemon);
     }, []);
+
+    const fetchPokemonData = async (pokemons: String[]) => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await fetch('https://tyradex.vercel.app/api/v1/pokemon');
+            if (!response.ok) {
+                const errorMessage = `Erreur HTTP! Statut: ${response.status}`;
+                console.error('Erreur lors de la récupération des données Pokémon:', errorMessage);
+                throw new Error(errorMessage);
+            }
+            const allPokemon: PokemonData[] = await response.json();
+            console.log('Données Pokémon récupérées avec succès:', allPokemon);
+
+            const filteredPokemon = allPokemon.filter((pokemon) =>
+                pokemons.some(
+                    (name) => pokemon.name.fr === name || pokemon.name.en === name
+                )
+            );
+
+            setPokemonData(filteredPokemon);
+        } catch (e: any) {
+            let message = 'Une erreur inconnue est survenue lors de la récupération des données Pokémon.';
+            if (e instanceof Error) {
+                message = e.message;
+            } else if (typeof e === 'string') {
+                message = e;
+            }
+            setError({ message });
+            console.error('Erreur lors de la récupération des données Pokémon:', e);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const postAnswer = async (body: { messages: ChatMessage[]}) => {
       try {
@@ -123,7 +127,6 @@ function Homepage() {
             body: JSON.stringify(body),
         };
         const response = await fetch('chat', requestOptions);
-        console.warn(requestOptions)
         if (!response.ok) {
             const errorMessage = `Erreur HTTP lors de l'envoi de la réponse: Statut ${response.status}`;
             console.error('Erreur lors de l\'envoi de la réponse:', errorMessage);
@@ -133,8 +136,10 @@ function Homepage() {
         setBodyPokemon({
             messages: [...body.messages, data] // Ajoute le nouveau message au tableau
         });
-        setCurrentQuestion(data.content);
-        console.log('Réponse envoyée et reçue avec succès:', data);
+
+        const botResponse = JSON.parse(data.content.substring(7, data.content.length - 3));
+
+        setCurrentQuestion(botResponse.type !== "answer" ? botResponse.content : `Penses tu au pokemon ${botResponse.content}?`);
     } catch (error) {
         console.error('Erreur lors de l\'envoi de la réponse ou du traitement de la réponse:', error);
     }
@@ -151,12 +156,26 @@ function Homepage() {
     };
 
     const modifyBodyAndPost = (answer: string) => {
-        postAnswer({
-            messages: [...bodyPokemon.messages, {
-                role: "user",
-                content: answer
-            }]
-        });
+        const lastMessage = bodyPokemon.messages[bodyPokemon.messages.length - 1];
+        const msgContent = lastMessage.content;
+        const msgContentJson = JSON.parse(msgContent.substring(7, msgContent.length - 3))
+        if (lastMessage.role === "assistant" && msgContentJson.type === "answer" && answer === "oui") {
+            const pokedex = [
+                ...pokemonNamesList, msgContentJson.content
+            ];
+            setPokemonNameList(pokedex);
+            fetchPokemonData(pokedex);
+            setBodyPokemon(iaContext);
+            postAnswer(iaContext);
+        } else {
+            postAnswer({
+                messages: [...bodyPokemon.messages, {
+                    role: "user",
+                    content: answer
+                }]
+            });
+        }
+
     };
 
     const handleCardClick = (pokemon: PokemonData) => {
@@ -207,7 +226,7 @@ function Homepage() {
             </div>
             <div className="homepage-background-container">
                 <img src={topBackgroundImg} alt="" />
-            </div>&
+            </div>
             <PopupPokemonDetails
                 pokemon={selectedPokemon}
                 isOpen={isPopupOpen}
